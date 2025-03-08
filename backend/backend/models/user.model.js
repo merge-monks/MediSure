55aa2c77ea7ccd591c2ebac6491515258ae3f2c6
Himanshu <hv308489@gmail.com>
2025-03-08 16:35:32 +0530

    userModel

---
+++ backend/backend/models/user.model.js
@@ -0,0 +1,34 @@
+mport mongoose from "mongoose";
+
+//Schema
+const userSchema = new mongoose.Schema(
+  {
+    displayName: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female"],
+    },
+    dob: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
