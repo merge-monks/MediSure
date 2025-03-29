6c70d4efdfd85c83e45b57f0e663a5fe9e465ea6
Himanshu <hv308489@gmail.com>
2025-03-29 11:37:45 +0530

    mongo,port

--- backend/backend/db/connectToMongodb.js
+++ backend/backend/db/connectToMongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect("mongodb+srv://Admin:8O7DIPsUZ1AG65hy@cluster0.rkdoows.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Connected To Database");
   } catch (err) {
     console.log(err);
