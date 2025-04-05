6b7d845ee3ca2159250ae93a33070f1af2f10466
Himanshu <hv308489@gmail.com>
2025-04-05 13:12:55 +0530

    cors

--- backend/backend/index.js
+++ backend/backend/index.js
@@ -29,13 +29,7 @@ clientWhatsapp.initialize();
 configDotenv();
 const app = express();
 
-// Enable CORS with multiple origins
-app.use(cors({
-  origin: ["http://localhost:5000", "http://localhost:3000", "http://localhost:5001", "http://3.110.188.8"],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors({ origin: '*' }))
 
 app.use(express.json());
 app.use(cookieParser());
@@ -43,7 +37,7 @@ app.use(cookieParser());
 app.use(
   session({
     name: "AuthCookie",
-    secret: "secret", // Replaced process.env.COOKIE_SECRET
+    secret: "secret",
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
@@ -52,17 +46,17 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      secure: false, // Replaced process.env.NODE_ENV === "PRODUCTION" ? true : false
-      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 day
+      secure: false, 
+      maxAge: 15 * 24 * 60 * 60 * 1000, 
     },
   })
 );
 
 app.use("/api/auth", authRoute);
-app.use("/api/medical", medicalRoute);  // Add the medical routes
+app.use("/api/medical", medicalRoute);  
 // app.use("/api/profile", profileRoute);
 
-// Add default route to check if server is running
+
 app.get("/", (req, res) => {
   res.status(200).json({ 
     message: "Server is running successfully", 
@@ -75,7 +69,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use("/api/uploads", express.static(path.join(__dirname, "../uploads")));
 
-app.listen(4000, () => { // Replaced process.env.PORT
+app.listen(4000, () => {
   connectToMongoDB();
   console.log(`Server running on http://localhost:4000`);
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "http://65.0.122.218",
       },
     },
   },
